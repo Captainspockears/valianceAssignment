2b2a6ebddb730768b7e549119fac4004c5695d50
captainspockears <ruthuparna1998@gmail.com>
2021-07-10 20:18:27 +0530

    Updated host by adding heroku

--- valianceAssignment/settings.py
+++ valianceAssignment/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b6c-gat%5$ez%-x362un#abma(7!3*g*&ecjiq6a9y-)f0!kkc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'valianceassignment.herokuapp.com']
 
 
 # Application definition
