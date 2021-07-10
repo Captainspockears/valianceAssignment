c51d90d2fb00d8c98b8b05d14ec0a8792f099d98
captainspockears <ruthuparna1998@gmail.com>
2021-07-10 20:25:35 +0530

    Added media and static root

--- valianceAssignment/urls.py
+++ valianceAssignment/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
+from django.views.static import serve
+from django.conf.urls import url
 
 from . import views
 
@@ -22,4 +26,9 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ]
