9447b915e335c055f02e4c45baef72b37aa2a8c9
captainspockears <ruthuparna1998@gmail.com>
2021-07-10 20:31:49 +0530

    Trying to fix deployment issue

--- valianceAssignment/settings.py
+++ valianceAssignment/settings.py
@@ -91,7 +91,8 @@ STATICFILES_DIRS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
